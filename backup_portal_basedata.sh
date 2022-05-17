935ef32e62ffeaa52f123b8afa71daf5165292b2
Norbert Truchsess <norbert.truchsess@t-online.de>
2022-05-17 21:55:35 +0200

    add backup script for portal scheme and basedata

---
+++ backup_portal_basedata.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+if [ -z $1 ]; then
+  echo "parameter 'hostname' missing";
+else
+  export DEVENV_SERVERNAME=$1
+  export DEVENV_IP=`getent hosts $DEVENV_SERVERNAME | awk '{ print $1 }'`
+  BASETABLES='(agreement_categories|app_status|company_application_status|company_role_descriptions|company_roles|company_status|company_user_status|consent_status|countries|document_types|identity_provider_categories|invitation_status|languages|use_cases)'
+  if [ -z "$DEVENV_IP" ]; then
+    echo "hostname $DEVENV_SERVERNAME unknown (run 'sudo ./set_wsl_hosts.sh' resp. 'sudo ./set_devhost_hosts.sh' first)"
+  else
+    docker run -it --rm -e PGPASSWORD=pwpostgres postgres pg_dump -h $DEVENV_IP -n portal -s -O -U postgres postgres > postgres/init/20-portal.sql
+    docker run -it --rm -e PGPASSWORD=pwportal postgres pg_dump -h $DEVENV_IP -a -t $BASETABLES -U portal postgres > postgres/init/21-portal-basedata.sql
+    docker run -it --rm -e PGPASSWORD=pwpostgres postgres pg_dump -h $DEVENV_IP -t '__efmigrations_history*' -U postgres postgres > postgres/init/90-public-efhistory.sql
+  fi
+fi
+
