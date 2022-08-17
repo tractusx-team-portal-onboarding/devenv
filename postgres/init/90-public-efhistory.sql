f1eafbfc1bf30076507c55db3023914c556ca392
Phil Schneider <info@philschneider.de>
2022-08-17 17:39:04 +0200

    CPLP-1255 add audit tables

--- postgres/init/90-public-efhistory.sql
+++ postgres/init/90-public-efhistory.sql
@@ -50,6 +50,7 @@ COPY public.__efmigrations_history_portal (migration_id, product_version) FROM s
 20220811060613_CPLP-1248-app-notification	6.0.4
 20220812133428_CPLP-1254-db-audit	6.0.4
 20220816061525_CPLP-1213-AddServices	6.0.4
+20220817151929_CPLP-1255-AuditCompanyApplications	6.0.4
 \.
 
 
