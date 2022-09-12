f0882859435be60ac54eff06339e3933f3124ad0
Phil Schneider <info@philschneider.de>
2022-09-12 10:11:17 +0200

    CPLP-1406 rename migration

--- postgres/init/90-public-efhistory.sql
+++ postgres/init/90-public-efhistory.sql
@@ -55,7 +55,7 @@ COPY public.__efmigrations_history_portal (migration_id, product_version) FROM s
 20220825071607_CPLP-1378-AddAppAssignedDocuments	6.0.4
 20220906074920_CPLP-1212-ChangeAppToOffer	6.0.4
 20220909133703_CPLP-1330-NotificationAppReleaseRequest	6.0.4
-20220911125152_CPLP-1406-AddAgreementAssignedApps	6.0.4
+20220911125152_CPLP-1406-AddAgreementAssignedOffers	6.0.4
 \.
 
 
