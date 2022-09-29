d7827b3f43ef2cd13586d08dfb06ccb9f6f71df4
Phil Schneider <info@philschneider.de>
2022-09-29 18:18:42 +0200

    CPLP-1440 updated migrations

--- postgres/init/90-public-efhistory.sql
+++ postgres/init/90-public-efhistory.sql
@@ -61,6 +61,8 @@ COPY public.__efmigrations_history_portal (migration_id, product_version) FROM s
 20220920100432_CPLP-831-AddConsentAssignedOffer	6.0.4
 20220921112615_CPLP-1409-ExpandNotificationTypes	6.0.4
 20220929071516_CPLP-1408-ServiceProviderCompanyDetail	6.0.4
+20220929160015_CPLP-1440-RemoveExistingTrigger	6.0.4
+20220929160513_CPLP-1440-DbAuditing	6.0.4
 \.
 
 
