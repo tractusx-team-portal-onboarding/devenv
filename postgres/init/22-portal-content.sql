be9fdb1870354893cb2229626c2b23d13492ba70
Phil Schneider <info@philschneider.de>
2022-11-28 10:07:06 +0100

    edit ids

--- postgres/init/22-portal-content.sql
+++ postgres/init/22-portal-content.sql
@@ -87,7 +87,7 @@ aa0a0000-7fbc-1f2f-817f-bce0502c1016	2	2022-09-18 00:00:00+00	2022-09-18 00:00:0
 aa0a0000-7fbc-1f2f-817f-bce0502c1017	4	2022-09-18 00:00:00+00	2022-09-18 00:00:00+00	\N	Terms & Conditions Service Provider	2dc4249f-b5ca-4d42-bef1-7a7a950a4f87	1aacde78-35ec-4df3-ba1e-f988cddcbbd9
 aa0a0000-7fbc-1f2f-817f-bce0502c1018	4	2022-09-18 00:00:00+00	2022-09-18 00:00:00+00	\N	Data Sharing Approval - allow CX to submit company data (company name, requester) to process the subscription	2dc4249f-b5ca-4d42-bef1-7a7a950a4f87	1aacde78-35ec-4df3-ba1e-f988cddcbbd9
 aa0a0000-7fbc-1f2f-817f-bce0502c1090	1	2022-01-01 00:00:00.388+00	2022-01-01 00:00:00.388+00	\N	I confirm that my company has successfully received a Catena-X onboarding certificate issued by an official Conformity Assessment Body (CAB). I awknowledge to upload the certificate. For more information please visit the association homepage (Link)	2dc4249f-b5ca-4d42-bef1-7a7a950a4f87	1aacde78-35ec-4df3-ba1e-f988cddcbbd9
-aa0a0000-7fbc-1f2f-817f-bce0502c1090	2	2022-01-01 00:00:00.388+00	2022-01-01 00:00:00.388+00	\N	I confirm that my company has successfully received a Catena-X onboarding certificate issued by an official Conformity Assessment Body (CAB). I awknowledge to upload the certificate. For more information please visit the association homepage (Link)	2dc4249f-b5ca-4d42-bef1-7a7a950a4f87	1aacde78-35ec-4df3-ba1e-f988cddcbbd9
+aa0a0000-7fbc-1f2f-817f-bce0502c1091	2	2022-01-01 00:00:00.388+00	2022-01-01 00:00:00.388+00	\N	I confirm that my company has successfully received a Catena-X onboarding certificate issued by an official Conformity Assessment Body (CAB). I awknowledge to upload the certificate. For more information please visit the association homepage (Link)	2dc4249f-b5ca-4d42-bef1-7a7a950a4f87	1aacde78-35ec-4df3-ba1e-f988cddcbbd9
 \.
 
 
@@ -1254,7 +1254,7 @@ b05d86e1-6c98-4619-85fa-9a425e0800b6	8cb12ea2-aed4-4d75-b041-ba297df3d2f2
 607818be-4978-41f4-bf63-fa8d2de51180	ec428950-8b64-4646-b336-28af869b5d73
 95fe4014-4d1b-47af-a22e-72a12f9470dc	a5b8b1de-7759-4620-9c87-6b6d74fb4fbc
 95fe4014-4d1b-47af-a22e-72a12f9470dc	ec428950-8b64-4646-b336-28af869b5d73
-16e4be9-0ff7-48fb-bb80-97d2e93fca14	a5b8b1de-7759-4620-9c87-6b6d74fb4fbc
+916e4be9-0ff7-48fb-bb80-97d2e93fca14	a5b8b1de-7759-4620-9c87-6b6d74fb4fbc
 3ff68c40-6581-4e48-a03a-2a6c1c5aac89	ec428950-8b64-4646-b336-28af869b5d73
 95fe4014-4d1b-47af-a22e-72a12f9470db	ec428950-8b64-4646-b336-28af869b5d73
 04231a30-df50-4d14-bb00-71ff012ae0cf	8cb12ea2-aed4-4d75-b041-ba297df3d2f2
