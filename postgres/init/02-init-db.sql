bb4ee8c6c37192334a058c6790d4ce172bae406a
Norbert Truchsess <norbert.truchsess@t-online.de>
2024-04-23 11:11:01 +0200

    add policy-hub

--- postgres/init/02-init-db.sql
+++ postgres/init/02-init-db.sql
@@ -2,6 +2,8 @@ CREATE SCHEMA portal;
 ALTER SCHEMA portal OWNER TO portal;
 CREATE SCHEMA provisioning;
 ALTER SCHEMA provisioning OWNER TO provisioning;
+CREATE SCHEMA "policy-hub";
+ALTER SCHEMA "policy-hub" OWNER TO hub;
 CREATE TABLE public.__efmigrations_history_portal (
     migration_id character varying(150) NOT NULL,
     product_version character varying(32) NOT NULL
@@ -12,3 +14,8 @@ CREATE TABLE public.__efmigrations_history_provisioning (
     product_version character varying(32) NOT NULL
 );
 ALTER TABLE public.__efmigrations_history_provisioning OWNER TO provisioning;
+CREATE TABLE public.__efmigrations_history_hub (
+    migration_id character varying(150) NOT NULL,
+    product_version character varying(32) NOT NULL
+);
+ALTER TABLE public.__efmigrations_history_hub OWNER TO hub;
